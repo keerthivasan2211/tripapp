7eee52db329d5cf596a57906e05da4d870650688
Keerthi Vasan <155253910+keerthivasan2211@users.noreply.github.com>
2026-02-21 10:44:46 +0530

    Fix import path for ProtectedRoute component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./Components/ProtectedRoute";
 import PublicRoute from "./Components/PublicRoute";
 import Home from "./pages/Home";
 import Oneday from "./pages/Oneday";
